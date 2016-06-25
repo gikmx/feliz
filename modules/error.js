fecc952c584dd3a5c04fc21ef0f06ceb9401500e
Héctor Menéndez <etor.mx@gmail.com>
2016-06-24 22:10:03 -0500

    Created a simple error generator.

---
+++ modules/error.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const ERRORS = [Error, TypeError, RangeError, ReferenceError, SyntaxError];
+
+function Exception(template, options){
+    const Instance = this;
+    if (!options || options.constructor !== Object) options = { message:String(options) };
+    Object
+        .keys(options)
+        .map(key => template = template.replace(`%${key}`, String(options[key])))
+    return new Instance(template);
+}
+
+module.exports = function(){
+    return [
+        [null        , Error          , '%message']                                    ,
+        ['type'      , TypeError      , 'Invalid %name. expecting %type; got: %data' ] ,
+        ['range'     , RangeError     , 'TODO: Add an error message here' ]            ,
+        ['reference' , ReferenceError , 'TODO: Add an error message here' ]            ,
+        ['syntax'    , SyntaxError    , 'TODO: Add an error message here' ]
+    ].reduce((acc, cur) => {
+        let type = cur.shift();
+        let bind = Exception.bind.apply(Exception, cur);
+        if (type) acc[type] = bind;
+        else acc = bind;
+        return acc;
+    }, null);
+}
