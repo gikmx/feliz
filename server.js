f23d52718dff9b519176baabb1fd8708fb66c536
Héctor Menéndez <etor.mx@gmail.com>
2016-06-27 08:19:10 -0500

    Renamed setup to server. It now handles basic server instantiation.

---
+++ server.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const Rx   = require('rxjs/Rx');
+const Hapi = require('hapi');
+
+module.exports = instance => Rx.Observable.create(observer => {
+
+    // get configurations and connections from plugins
+    const plugins = instance.options.plugins
+        .map(plugin => {
+            instance.options.config = instance.util
+                .object(instance.options.config)
+                .merge(plugin.config);
+            instance.options.connection = instance.util
+                .object(instance.options.connection)
+                .merge(plugin.connection);
+            return plugin.register;
+        });
+
+    // Set configuration and connection defaults
+    instance.options.config = instance.util
+        .object({
+            connections: {
+                routes: {
+                    files: { relativeTo: instance.options.root }
+                }
+            }
+        })
+        .merge(instance.options.config);
+    instance.options.connection = instance.util
+        .object({
+            port: process.env.PORT || 8000
+        })
+        .merge(instance.options.connection);
+
+
+    instance.server = new Hapi.Server(instance.options.config);
+    instance.server.connection(instance.options.connection);
+
+    plugins.map(plugin => instance.server.register(plugin))
+
+    instance.server.start(err => {
+        if (err) return observer.error(err);
+        observer.next(instance);
+
+        process.on('SIGINT', function(){
+            instance.server.stop(err => {
+                if (err) return observer.error(err);
+                observer.complete();
+                process.exit(0);
+            });
+        })
+    })
+});
+
--- setup.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-module.exports = function(instance){
-
-    return instance;;
-}
