25f285c0f63492b9109df7ff7ee1bed44f662aa4
Héctor Menéndez <etor.mx@gmail.com>
2016-06-27 05:11:08 -0500

    Updated the module loader for new changes in the way modules (core/base) & options are loaded

--- index.js
+++ index.js
@@ -8,53 +8,87 @@ const Rx = require('rxjs/Rx');
 const Setup = require('./setup');
 const Config = require('./config.json');
 
-// The folder where the core modules reside
-const PATH_MODULES = PATH.join(__dirname, 'modules');
+// Folders where the modules reside
+const PATH_CORE = PATH.join(__dirname, 'core');
+const PATH_BASE = PATH.join(__dirname, 'base');
 
 // Convert Node functions into observable generators
 const rxReaddir = Rx.Observable.bindNodeCallback(FS.readdir);
 const rxAccess  = Rx.Observable.bindNodeCallback(FS.access);
 
+// Module loader
+const rxModules = path =>
+    rxAccess(path, FS.R_OK)
+    .mergeMap(()=> rxReaddir(path))
+    .mergeMap(filenames => filenames.sort())
+    .map(filename => ({
+        name: PATH
+            .basename(filename, PATH.extname(filename))
+            .replace(/^\d+\-/,''),
+        data: require(PATH.join(path, filename))
+    }));
+
 module.exports = (options={}) => Rx.Observable.create(observer => {
 
     // temporary object that will store the main instance while being created
     // TODO: Find a way to get rid of this, instace$ should store this temp data.
-    let tmp = {};
+    let fai = {};
 
     const options$ = Rx.Observable.of({ name: 'options', data: options });
-
-    const modules$ = rxAccess(PATH_MODULES, FS.R_OK)
-        .mergeMap(()=> rxReaddir(PATH_MODULES))
-        .mergeMap(filenames => filenames.sort())
-        .map(filename => ({
-            name: PATH
-                .basename(filename, PATH.extname(filename))
-                .replace(/^\d+\-/,''),
-            data: require(PATH.join(PATH_MODULES, filename))
-        }));
+    const core_modules$ = rxModules(PATH_CORE);
+    const base_modules$ = rxModules(PATH_BASE);
 
     const instance$ = Rx.Observable
-        .merge(modules$, options$)
-        .mergeMap(item => {
+        .concat(core_modules$, options$, base_modules$)
+        // Make sure every item is resolved in order before proceeding.
+        .concatMap(item => {
             if (typeof item.data !== 'function') return Rx.Observable.of(item);
-            else return Rx.Observable.create(obs => {
-                // instantiate each item and send the current value of the main instance
-                item.data(tmp, item.name).subscribe(
-                    data => {
-                        tmp[item.name] = data;
-                        obs.next({ data, name:item.name });
-                    },
-                    err => obs.error(err),
-                    ()  => obs.complete()
+            return Rx.Observable.create(obs => {
+                item.data(fai, item.name).subscribe(
+                    data => obs.next({ data, name:item.name }),
+                    err  => obs.error(err),
+                    ()   => obs.complete()
                 );
             });
+        })
+        // validate options before continuing to parse everything else
+        // core modules will be available at this point
+        .concatMap(item => {
+            // Populate tmp instance and continue if not dealing with options.
+            fai[item.name] = item.data;
+            if (item.name !== 'options') return Rx.Observable.of(item);
+            // synchronouse validations
+            if (!fai.util.is(item.data).object()) throw fai.error.type({
+                name: 'options',
+                type: 'Object',
+                data: item.data
+            });
+            if (!fai.util.is(item.data.root).string()) throw fai.error.type({
+                name: 'options.root',
+                type: 'String',
+                data: item.data.root
+            });
+            if (item.data.root[0] !== PATH.sep)
+                item.data.root = PATH.resolve(item.data.root);
+            // async validations
+            const root_opt$ = Rx.Observable
+                .of(item.data.root)
+                .concatMap(root => rxAccess(root, FS.R_OK))
+                .catch(()=> {
+                    throw fai.error.type({
+                        name: 'options.root',
+                        type: 'existent path',
+                        data: item.data.root
+                    });
+                });
+            return root_opt$.mapTo(item);
         })
         .reduce((instance, item) => {
             instance[item.name] = item.data;
             return instance;
         }, {})
         // we no longer need the temporary instance, get rid of it.
-        .do(() => { tmp = undefined });
+        .do(() => { fai = undefined });
 
     function onReady(type, instance){
         // Handle errors or completions
