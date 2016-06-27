8c6df2fc364de47fc1a7e0b65c52a9a4266dbffc
Héctor Menéndez <etor.mx@gmail.com>
2016-06-27 16:22:01 -0500

    Addded base module for routes validation

---
+++ base/01-routes.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const PATH = require('path');
+const FS   = require('fs');
+
+const Rx = require('rxjs/Rx');
+
+const rxAccess  = Rx.Observable.bindNodeCallback(FS.access);
+const rxReaddir = Rx.Observable.bindNodeCallback(FS.readdir);
+const rxStat    = Rx.Observable.bindNodeCallback(FS.stat);
+const rxReadAll = (parent, root=false) => {
+    if (!root) root = parent;
+    const items$ = rxReaddir(parent)
+        .mergeAll()
+        .mergeMap(filename => Rx.Observable.create(observer => {
+            const path = PATH.join(parent, filename);
+            let name = `${parent.replace(root, '')}${path.replace(parent, '')}`
+                .slice(1)
+                .slice(0, -1 * PATH.extname(path).length)
+            if (name.indexOf('/index') === name.length -6) name = name.slice(0, -6);
+            rxStat(path).subscribe(
+                stats => observer.next({ path, name, isdir: stats.isDirectory() }),
+                err   => observer.error(err),
+                ()    => observer.complete()
+            );
+        }));
+    const files$ = items$.filter(item => !item.isdir);
+    const dir$   = items$
+        .filter(item => item.isdir)
+        .mergeMap(item => rxReadAll(item.path, root));
+    return Rx.Observable
+        .merge(files$, dir$)
+        .map(item => ({ path:item.path, name:item.name }))
+};
+
+module.exports = instance => {
+
+    const PATH_ROUTES = PATH.join(instance.path.app.root, 'routes' + instance.path.ext);
+
+    // determine which bundles are available (but don't load them)
+    const bundles$ = rxReadAll(instance.path.app.bundles).toArray();
+
+    const route$ = rxAccess(PATH_ROUTES, FS.R_OK)
+        .catch(() => { throw instance.error('Missing Routes file'); })
+        .mapTo(require(PATH_ROUTES))
+        .mergeMap(routes => Object
+            .keys(routes)
+            .map(path => Object.assign({path}, routes[path]))
+        )
+
+    return bundles$
+        .switchMap(bundles => route$
+            .map(route => {
+                const bundle = bundles
+                    .map(item => item.name)
+                    .indexOf(route.bundle);
+                if (bundle === -1) throw instance.error(`Invalid Bundle: ${route.bundle}`);
+                route.bundle = bundles[bundle];
+                route.bundle.func = require(route.bundle.path);
+                return route;
+            })
+        )
+        .toArray()
+}
