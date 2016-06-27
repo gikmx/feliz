29c8fef988521dfd202c5dcc893246e301a9010b
Héctor Menéndez <etor.mx@gmail.com>
2016-06-26 22:34:14 -0500

    Updated main instance logic so it sends itself to each module.

--- index.js
+++ index.js
@@ -1,13 +1,73 @@
 'use strict';
 
+const FS   = require('fs');
+const PATH = require('path');
+
 const Rx = require('rxjs/Rx');
+
+const Setup = require('./setup');
+const Config = require('./config.json');
+
+// The folder where the core modules reside
+const PATH_MODULES = PATH.join(__dirname, 'modules');
+
+// Convert Node functions into observable generators
+const rxReaddir = Rx.Observable.bindNodeCallback(FS.readdir);
+const rxAccess  = Rx.Observable.bindNodeCallback(FS.access);
 
 module.exports = (options={}) => Rx.Observable.create(observer => {
 
-    observer.next(null);
-    observer.complete();
+    // temporary object that will store the main instance while being created
+    // TODO: Find a way to get rid of this, instace$ should store this temp data.
+    let tmp = {};
 
-    return function(){
-        process.stdout.write('»\n');
+    const options$ = Rx.Observable.of({ name: 'options', data: options });
+
+    const modules$ = rxAccess(PATH_MODULES, FS.R_OK)
+        .mergeMap(()=> rxReaddir(PATH_MODULES))
+        .mergeMap(filenames => filenames.sort())
+        .map(filename => ({
+            name: PATH
+                .basename(filename, PATH.extname(filename))
+                .replace(/^\d+\-/,''),
+            data: require(PATH.join(PATH_MODULES, filename))
+        }));
+
+    const instance$ = Rx.Observable
+        .merge(modules$, options$)
+        .mergeMap(item => {
+            if (typeof item.data !== 'function') return Rx.Observable.of(item);
+            else return Rx.Observable.create(obs => {
+                // instantiate each item and send the current value of the main instance
+                item.data(tmp, item.name).subscribe(
+                    data => {
+                        tmp[item.name] = data;
+                        obs.next({ data, name:item.name });
+                    },
+                    err => obs.error(err),
+                    ()  => obs.complete()
+                );
+            });
+        })
+        .reduce((instance, item) => {
+            instance[item.name] = item.data;
+            return instance;
+        }, {})
+        // we no longer need the temporary instance, get rid of it.
+        .do(() => { tmp = undefined });
+
+    function onReady(type, instance){
+        // Handle errors or completions
+        if (type != 'ready') return this[type](instance);
+        const result = Setup(instance);
+        if (result) instance = result;;
+        this.next(instance);
     }
+
+    instance$.subscribe(
+        onReady.bind(observer, 'ready'),
+        onReady.bind(observer, 'error'),
+        onReady.bind(observer, 'complete')
+    );
 });
+
---
+++ setup.js
@@ -0,0 +1,6 @@
+'use strict';
+
+module.exports = function(instance){
+
+    return instance;;
+}
