bee9b766dc11a07de655746398ad95b193119f21
Héctor Menéndez <etor.mx@gmail.com>
2016-08-03 22:10:02 -0500

    Added basic tests for the new feliz instance.

--- test/feliz.js
+++ test/feliz.js
@@ -1,11 +1,42 @@
 'use strict';
 
-const Test  = require('tape');
-const Feliz = require('../lib/feliz');
+const Tape       = require('tape');
+const RxJS       = require('rxjs')
+const FelizError = require('feliz.error');
+const FelizUtil  = require('feliz.util');
 
-Test('this should pass', function(tape){
-    tape.doesNotThrow(()=> {
-        Feliz()
-        tape.end();
-    })
+const Package = require('../package.json');
+const Feliz   = require('../lib/feliz');
+
+Tape('feliz', t => {
+
+    t.equal(typeof Feliz, 'function', 'should be a function');
+
+    t.end();
 })
+
+Tape('feliz.observable', t => {
+
+    t.equal(typeof Feliz.observable, 'function', 'should be a function');
+
+    const fn1 = Feliz.observable.toString();
+    const fn2 = RxJS.Observable.toString();
+    t.equal(fn1, fn2, 'should be the same function as rxjs');
+
+    t.end();
+})
+
+Tape('feliz.error', t => {
+    t.deepEqual(Feliz.error, FelizError, 'should be the feliz.error module');
+    t.end();
+});
+
+Tape('feliz.util', t => {
+    t.deepEqual(Feliz.util, FelizUtil, 'should be the feliz.util module');
+    t.end();
+});
+
+Tape('feliz.package', t => {
+    t.deepEqual(Feliz.package, Package, 'should be the same object as package.json');
+    t.end();
+});
